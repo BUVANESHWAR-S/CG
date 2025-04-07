1eb98f6f166d7374685d8137af4e29c21329aef3
BUVANESHWAR S <buvansrinivasan@gmail.com>
2025-04-07 14:26:20 +0530

    deleted

--- day1_4/class_/student_ll.java
+++ day1_4/class_/student_ll.java
@@ -1,1 +1,21 @@
-package class_;
+package day1_4.class_;
+
+public class student_ll {
+
+    private Node head;
+    private Node tail;
+
+    private class Node{
+        private int Roll_num, age;
+        String Name;
+        char grade;
+
+        public Node(String Name, int Roll_num, int age, char grade){
+            this.Name = Name;
+            this.Roll_num = Roll_num;
+            this.age = age;
+            this.grade = grade;
+        }
+    }
+    
+}
